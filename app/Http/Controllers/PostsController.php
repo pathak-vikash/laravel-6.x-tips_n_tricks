2acba4b3bb3d677288d0e9d41ac5e97a170b825d
Vikash Pathak <vikashh.pathak@gmail.com>
2019-11-21 09:46:23 +0530

    Added in routes - Route::apiResource('posts', 'PostsController');

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Post;
 use Illuminate\Http\Request;
 
-class PostController extends Controller
+class PostsController extends Controller
 {
     /**
      * Display a listing of the resource.
